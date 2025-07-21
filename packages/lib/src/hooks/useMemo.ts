be9d9b951adb75e035b1d590be5c8ee4125e0652
heojungseok <heojungseok@heojungseokui-MacBookAir.local>
2025-07-21 23:38:13 +0900

    feat: useMemo 구형
    - useRef 활용
    - 메모이제이션, 의존성 비교

--- packages/lib/src/hooks/useMemo.ts
+++ packages/lib/src/hooks/useMemo.ts
@@ -3,14 +3,13 @@ import { shallowEquals } from "../equals";
 import { useRef } from "./useRef";
 
 export function useMemo<T>(factory: () => T, _deps: DependencyList, _equals = shallowEquals): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  const memoizatedRef = useRef(null);
-  if (memoizatedRef.current === null || !_equals(memoizatedRef.current.deps, _deps)) {
-    memoizatedRef.current = { deps: _deps, result: factory() };
-    return memoizatedRef.current.result;
-  }
+  // 단 한 번만 초기화. 컴포넌트가 사라지기 전까지 유지.
+  const memoizatedRef = useRef<{ deps: DependencyList; result: T } | null>(null);
 
-  if (_equals(memoizatedRef.current.deps, _deps) && _deps) {
-    return memoizatedRef.current.result;
+  if (memoizatedRef.current === null || !_equals(memoizatedRef.current.deps, _deps)) {
+    // 만약 새로운 값으로 업데이트 해야한다면 랜더링을 유발하지 않는 방식으로..
+    memoizatedRef.current = { deps: _deps, result: factory() };
   }
+  // 마지막엔 저장된 result 반환
+  return memoizatedRef.current.result;
 }
